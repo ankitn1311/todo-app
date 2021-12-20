a0f9e937cf6d1bc7f683a39728e4b0d5bfcb9cb8
Himanshu-Negi-Dev <himanshu.dev7350@gmail.com>
2021-12-20 14:45:39 +0530

    redux setup complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,22 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import SignUp from "./components/sign-up/SignUp";
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-800 text-gray-300">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<div>Hello from home</div>} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="about" element={<div>Hello from about</div>} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Hello from home</div>} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="about" element={<div>Hello from about</div>} />
+        </Routes>
+    </Provider>
+      </div>
+
   );
 }
 
