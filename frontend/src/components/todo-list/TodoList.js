1b1cd9016661244124fec913d7f3ea0d5caf0b5b
Ankit Negi <ankitn1311@gmail.com>
2021-12-25 00:07:10 +0530

    Edit Todo Done

--- frontend/src/components/todo-list/TodoList.js
+++ frontend/src/components/todo-list/TodoList.js
@@ -3,50 +3,58 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { clearCurrentTodo, createTodo, getTodos, updateTodo } from "../../redux/actions/todos";
+import {
+  clearCurrentTodo,
+  createTodo,
+  getTodos,
+  updateTodo,
+} from "../../redux/actions/todos";
 import Input from "../common/form/Input";
 import Paper from "../common/ui/Paper";
 import Todo from "./todo/Todo";
 const TodoList = () => {
-  const [buttonName, setButtonName] = useState('Add')
   const { todos, currentTodo } = useSelector((state) => state.todos);
+
+  const [initialValues, setInitialValues] = useState({
+    title: "",
+    description: "",
+  });
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTodos());
   }, []);
 
   const formik = useFormik({
-    initialValues: {
-      title: "",
-      description: "",
-    },
+    initialValues,
     validationSchema: Yup.object({
       title: Yup.string().required("Required"),
       description: Yup.string().required("Required"),
     }),
+    enableReinitialize: true,
     onSubmit: (values) => {
-      if(currentTodo){
-        const payload = {_id: currentTodo._id, title: values.title, description: values.description}
-        dispatch(updateTodo(payload))
-        setButtonName('add')
+      if (currentTodo) {
+        const payload = {
+          _id: currentTodo._id,
+          title: values.title,
+          description: values.description,
+        };
+        dispatch(updateTodo(payload));
         dispatch(clearCurrentTodo());
-      }else{
-        dispatch(createTodo(values))
+      } else {
+        dispatch(createTodo(values));
       }
-      formik.values.title=""
-      formik.values.description=""
+      formik.values.title = "";
+      formik.values.description = "";
     },
   });
-
 
   return (
     <div className="flex items-start justify-center h-full p-4">
       <Paper className="w-full overflow-hidden rounded-xl sm:w-1/2 lg:w-1/3 xl:w-1/3">
-        <form 
-        className="flex flex-col items-end gap-6 p-6 bg-teal-500/30"
-        onSubmit = {formik.handleSubmit}
-        >
-
+        <form
+          className="flex flex-col items-end gap-6 p-6 bg-teal-500/30"
+          onSubmit={formik.handleSubmit}>
           <Input
             placeholder="Enter title"
             id="title"
@@ -73,12 +81,12 @@ const TodoList = () => {
           <button
             type="submit"
             className="px-4 py-2 bg-teal-800 rounded-lg text-slate-50 dark:text-slate-200 dark:bg-slate-800 hover:bg-slate-700 ">
-           {buttonName}
+            {!currentTodo ? "add" : "update"}
           </button>
         </form>
         <div className="flex flex-col divide-y-1 divide-slate-700">
           {todos.map((todo) => (
-            <Todo todo={todo} setButtonName={setButtonName}/>
+            <Todo todo={todo} changeValues={setInitialValues} />
           ))}
         </div>
       </Paper>
--- frontend/src/components/todo-list/todo/Todo.js
+++ frontend/src/components/todo-list/todo/Todo.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteTodo, setCurrentTodo, updateTodo } from "../../../redux/actions/todos";
-const Todo = ({ todo, data, setButtonName }) => {
+import {
+  clearCurrentTodo,
+  deleteTodo,
+  setCurrentTodo,
+  updateTodo,
+} from "../../../redux/actions/todos";
+const Todo = ({ todo, changeValues }) => {
   const dispatch = useDispatch();
 
-  const handleDelete = () =>{
-    dispatch(deleteTodo(todo._id))
-  }
+  const handleDelete = () => {
+    dispatch(clearCurrentTodo());
+    dispatch(deleteTodo(todo._id));
+  };
 
-  const handleEdit = () =>{
-    setButtonName('update')
-    dispatch(setCurrentTodo(todo))
-  }
+  const handleEdit = () => {
+    changeValues({ title: todo.title, description: todo.description });
+    dispatch(setCurrentTodo(todo));
+  };
   return (
     <div className="flex flex-row items-center justify-between gap-4 p-6 ">
       <div className="flex flex-col start">
@@ -21,10 +27,14 @@ const Todo = ({ todo, data, setButtonName }) => {
         <div className="text-sm dark:text-gray-400">{todo.description}</div>
       </div>
       <div className="flex flex-col items-end text-sm font-semibold">
-        <button className="tracking-widest text-red-700 uppercase hover:text-red-500 dark:text-red-400 dark:hover:text-red-300" onClick={handleDelete}>
+        <button
+          className="tracking-widest text-red-700 uppercase hover:text-red-500 dark:text-red-400 dark:hover:text-red-300"
+          onClick={handleDelete}>
           delete
         </button>
-        <button className="tracking-widest text-teal-800 uppercase hover:text-teal-500 dark:text-teal-400 dark:hover:text-teal-300" onClick={handleEdit}>
+        <button
+          className="tracking-widest text-teal-800 uppercase hover:text-teal-500 dark:text-teal-400 dark:hover:text-teal-300"
+          onClick={handleEdit}>
           edit
         </button>
       </div>
