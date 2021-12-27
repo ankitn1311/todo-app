f41d6dcbae094f1ca639ca5f4c2c634f81428f73
Ankit Negi <ankitn1311@gmail.com>
2021-12-27 14:56:35 +0530

    Restructured entire application structure

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -12,6 +13,12 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 app.use("/api/", authRoute);
 app.use("/api/", userRoute);
 app.use("/api/", todoRoute);
